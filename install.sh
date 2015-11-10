14cf1653edbe300ab8f1937f02af43fb6a7563b3
Matthieu M <matthieu@ma2t.com>
2015-11-10 15:11:28 +0100

    Fix install.sh

--- install.sh
+++ install.sh
@@ -92,18 +92,18 @@ else
 	echo -e "The detected Linux Distribution is: " $ID-$VERSION_ID
 fi
 echo
-if [ -n "$DISTRO" ]; then
+#if [ -n "$DISTRO" ]; then
 #	read -p "Is this correct? (y/n)" -n 1 -r
 #	echo    # (optional) move to a new line
 #	if [[ ! $REPLY =~ ^[Yy]$ ]]
 #		then
 #		exit 1
 #	fi
-else
-	echo -e "Sorry but your System is not supported by this script, if you want your system supported "
-	echo -e "open an issue on GitHub: https://github.com/servisys/ispconfig_setup"
-	exit 1
-fi
+#else
+#	echo -e "Sorry but your System is not supported by this script, if you want your system supported "
+#	echo -e "open an issue on GitHub: https://github.com/servisys/ispconfig_setup"
+#	exit 1
+#fi
 
 
 if [ -f /etc/debian_version ]; then
